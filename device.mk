06d44b3ba2fb0d9a757cbf2c577edb74e0794d76
eraselk <eraselk@proton.me>
2024-10-19 06:45:03 +0800

    device.mk: build graphics library

--- device.mk
+++ device.mk
@@ -79,11 +79,13 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Additional Target Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
     android.hardware.keymaster@4.1 \
+    android.hardware.graphics.common@1.0 \
     libion \
     libxml2
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.graphics.common@1.0 \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
 
